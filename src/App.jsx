fc8c0224ed6a093b17677e370236162ba128403c
Vicky Kumar <89451580+Syz68@users.noreply.github.com>
2025-02-12 12:42:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import InvisalignAtAster from './invisalign/Invisalign'
+
+
+function App() {
+  return (
+   <>
+    <InvisalignAtAster/>
+   </>
+  )
+}
+
+export default App
